9fdf0c5295cb7f7129983b3caa9f41acded461d2
JunaidAli777 <syedjunaidali.ali@gmail.com>
2024-07-23 01:11:13 +0530

    nav-bar theme changed to blue

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,12 +6,12 @@ import {
   Route,
   RouterProvider
   } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
+//import 'bootstrap/dist/css/bootstrap.min.css';
 import { PayPalScriptProvider } from '@paypal/react-paypal-js';
 import { HelmetProvider } from 'react-helmet-async';
 import { Provider } from 'react-redux';
 import store from './store';
-//import './assets/styles/bootstrap.custom.css';
+import './assets/styles/bootstrap.custom.css';
 import './assets/styles/index.css';
 import App from './App';
 import PrivateRoute from './components/PrivateRoute';
